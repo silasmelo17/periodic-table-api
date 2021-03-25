e085cc8376062abb083295baecc5f7667d407332
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-03-25 05:45:57 -0300

    create mongoose connect

---
+++ src/Util/database.js
@@ -0,0 +1,12 @@
+
+const mongoose = require('mongoose')
+
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+
+
+module.exports = mongoose
